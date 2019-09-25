a39d921db1671bef7f2512d6eb9b30f949aec400
hakiba <h031203yama@gmail.com>
2019-09-25 16:03:55 +0900

    test: 利用回数に応じたテストを追加

--- ex01-business-rules/src/test/java/kata/ex01/DiscountServiceTest.java
+++ ex01-business-rules/src/test/java/kata/ex01/DiscountServiceTest.java
@@ -28,7 +28,7 @@ public class DiscountServiceTest {
     }
 
     @Test
-    public void test平日朝夕割引() {
+    public void test平日朝夕割引_利用回数10回以上で5割引きされること() {
         HighwayDrive drive = new HighwayDrive();
         drive.setEnteredAt(LocalDateTime.of(2016, 3, 31, 23, 0));
         drive.setExitedAt(LocalDateTime.of(2016, 4, 1, 6, 30));
@@ -37,6 +37,39 @@ public class DiscountServiceTest {
         drive.setRouteType(RURAL);
 
         assertThat(discountService.calc(drive)).isEqualTo(50);
+    }
+
+    @Test
+    public void test平日朝夕割引_利用回数5回以上9回以下で3割引きされること() {
+        HighwayDrive drive = new HighwayDrive();
+        drive.setEnteredAt(LocalDateTime.of(2016, 3, 31, 23, 0));
+        drive.setExitedAt(LocalDateTime.of(2016, 4, 1, 6, 30));
+        drive.setDriver(driver(9));
+        drive.setVehicleFamily(STANDARD);
+        drive.setRouteType(RURAL);
+
+        assertThat(discountService.calc(drive)).isEqualTo(30);
+
+        HighwayDrive drive2 = new HighwayDrive();
+        drive2.setEnteredAt(LocalDateTime.of(2016, 3, 31, 23, 0));
+        drive2.setExitedAt(LocalDateTime.of(2016, 4, 1, 6, 30));
+        drive2.setDriver(driver(5));
+        drive2.setVehicleFamily(STANDARD);
+        drive2.setRouteType(RURAL);
+
+        assertThat(discountService.calc(drive2)).isEqualTo(30);
+    }
+
+    @Test
+    public void test平日朝夕割引_利用回数が5回未満の場合割引されないこと() {
+        HighwayDrive drive = new HighwayDrive();
+        drive.setEnteredAt(LocalDateTime.of(2016, 3, 31, 23, 0));
+        drive.setExitedAt(LocalDateTime.of(2016, 4, 1, 6, 30));
+        drive.setDriver(driver(4));
+        drive.setVehicleFamily(STANDARD);
+        drive.setRouteType(RURAL);
+
+        assertThat(discountService.calc(drive)).isEqualTo(0);
     }
 
     @Test
