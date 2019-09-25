5324dedd9384739f18d44fe61d353e9446e5d666
hakiba <h031203yama@gmail.com>
2019-09-25 15:59:42 +0900

    feature: 平日朝夕割引50%ディスカウントを実装

--- ex01-business-rules/src/main/java/kata/ex01/DiscountServiceImpl.java
+++ ex01-business-rules/src/main/java/kata/ex01/DiscountServiceImpl.java
@@ -1,6 +1,8 @@
 package kata.ex01;
 
 import kata.ex01.model.HighwayDrive;
+import kata.ex01.model.RouteType;
+import kata.ex01.util.HolidayUtils;
 
 /**
  * @author kawasima
@@ -8,6 +10,26 @@ import kata.ex01.model.HighwayDrive;
 public class DiscountServiceImpl implements DiscountService {
     @Override
     public long calc(HighwayDrive drive) {
+        return discountByWeekDaysRule(drive);
+    }
+
+    private long discountByWeekDaysRule(HighwayDrive drive) {
+        boolean isNotHoliday = !HolidayUtils.isHoliday(drive.getEnteredAt().toLocalDate());
+        boolean isMorning =
+                6 <= drive.getEnteredAt().getHour() && drive.getEnteredAt().getHour() <= 9
+                || 6 <= drive.getExitedAt().getHour() && drive.getExitedAt().getHour() <= 9;
+        boolean isEvening =
+                17 <= drive.getEnteredAt().getHour() && drive.getEnteredAt().getHour() <= 20
+                        || 17 <= drive.getExitedAt().getHour() && drive.getExitedAt().getHour() <= 20;
+        boolean isRural = RouteType.RURAL == drive.getRouteType();
+        if (isNotHoliday && (isMorning || isEvening) && isRural) {
+            int countPerMonth = drive.getDriver().getCountPerMonth();
+            if (countPerMonth >= 10) {
+                return 50;
+            } else if (countPerMonth >= 5) {
+                return 30;
+            }
+        }
         return 0;
     }
 }
