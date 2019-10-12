ebdbfeee9f6fa1b618b2ebfce3abb9957d6e6125
ryo-chin <h031203yama@gmail.com>
2019-10-12 23:04:22 +0900

    ex02 とりあえずテストが通るように実装

--- ex02-pluggable/src/main/java/kata/ex02/OrderServiceImpl.java
+++ ex02-pluggable/src/main/java/kata/ex02/OrderServiceImpl.java
@@ -2,24 +2,35 @@ package kata.ex02;
 
 import com.google.inject.Inject;
 import kata.ex02.model.Order;
+import kata.ex02.model.OrderDetail;
 import kata.ex02.repository.OrderRepository;
 import kata.ex02.util.ApiSender;
 import kata.ex02.util.MailSender;
+
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class OrderServiceImpl implements OrderService {
     private OrderRepository repository;
     private ApiSender apiSender;
     private MailSender mailSender;
+    private Map<String, Consumer<OrderDetail>> notificationOrderMap;
 
     @Inject
     public OrderServiceImpl(OrderRepository repository, ApiSender apiSender, MailSender mailSender) {
         this.repository = repository;
         this.apiSender = apiSender;
         this.mailSender = mailSender;
+        this.notificationOrderMap = Map.of(
+                "A", apiSender::send,
+                "B", mailSender::send
+        );
     }
 
     @Override
     public void order(Order order) {
         repository.save(order);
+        order.getOrderDetails()
+                .forEach(orderDetail -> notificationOrderMap.get(orderDetail.getProductProvider().getName()).accept(orderDetail));
     }
 }
